c467f57bfbd94f3ead2703387f883a313b218b28
reves <ricardo.reves@gmail.com>
2019-05-09 19:10:55 +0200

    add styleurl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
 })
 export class AppComponent implements OnInit {
 
