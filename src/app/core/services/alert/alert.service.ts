d08bd612efaed6e9b7c1ae1b0d2ee6dcea9c20e5
reves <ricardo.reves@gmail.com>
2019-05-22 22:16:28 +0200

    del toast import

--- src/app/core/services/alert/alert.service.ts
+++ src/app/core/services/alert/alert.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AlertController, ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
