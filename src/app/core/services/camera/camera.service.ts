3eff6ab2353854810ca92c43891ba8672ff896ee
reves <ricardo.reves@gmail.com>
2019-05-22 22:17:04 +0200

    improve takePicture method

--- src/app/core/services/camera/camera.service.ts
+++ src/app/core/services/camera/camera.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Plugins, CameraResultType, CameraSource, CameraOptions } from '@capacitor/core';
+import { Capacitor, Plugins, CameraResultType, CameraSource, CameraOptions, CameraDirection } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root'
@@ -9,21 +9,20 @@ export class CameraService {
   constructor() { }
 
   public async takePicture() {
+    // docs: https://capacitor.ionicframework.com/docs/apis/camera/
     const { Camera } = Plugins;
     const options: CameraOptions = {
       quality: 90,
       allowEditing: true,
-      resultType: CameraResultType.Base64,
-      source: CameraSource.Camera
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+      direction: CameraDirection.Rear
     }
-    const image = await Camera.getPhoto(options).catch(err => err);
-    console.log('getPhoto', image);
-    if (!image) {
-      return;
-    }
+    const image = await Camera.getPhoto(options).catch(err => console.error(err))
+
     // Example of using the Base64 return type. It's recommended to use CameraResultType.Uri
     // instead for performance reasons when showing large, or a large amount of images.
     //this.image = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.base64Data));
-    return await image.dataUrl
+    return await (image && image.dataUrl) || null
   }
 }
