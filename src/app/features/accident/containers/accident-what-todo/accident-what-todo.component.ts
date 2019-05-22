77edd583996d542e9f3e8f0926157429721d61cf
reves <ricardo.reves@gmail.com>
2019-05-22 22:20:07 +0200

    create accident what todo component

---
+++ src/app/features/accident/containers/accident-what-todo/accident-what-todo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accident-what-todo',
+  templateUrl: './accident-what-todo.component.html',
+  styleUrls: ['./accident-what-todo.component.scss'],
+})
+export class AccidentWhatTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
