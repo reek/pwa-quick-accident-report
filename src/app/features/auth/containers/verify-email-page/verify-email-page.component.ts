d9b80ff63f587e19f0123f2cb37d755c29ac2a50
reves <ricardo.reves@gmail.com>
2019-05-28 17:11:21 +0200

    create verify email component

---
+++ src/app/features/auth/containers/verify-email-page/verify-email-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verify-email-page',
+  templateUrl: './verify-email-page.component.html',
+  styleUrls: ['./verify-email-page.component.scss'],
+})
+export class VerifyEmailPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
