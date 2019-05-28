ef214f060369fbe3f5db0e5e265649cd30a6b2d6
reves <ricardo.reves@gmail.com>
2019-05-28 18:20:18 +0200

    update components  import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,22 +4,26 @@ import { IonicModule } from '@ionic/angular';
 import { SearchFilterPipe } from './pipes/search-filter/search-filter.pipe';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './ui/header/header.component';
-import { ImageComponent } from './ui/image/image.component';
 import { TakePictureComponent } from './ui/take-picture/take-picture.component';
 import { OlMapComponent } from './ui/ol-map/ol-map.component';
 import { PersonFormComponent } from './ui/person-form/person-form.component';
-import { AddressFormComponent } from './ui/address-form/address-form.component';
+import { ItemTitleComponent } from './ui/item-title/item-title.component';
+import { SearchAddressComponent } from './ui/search-address/search-address.component';
+import { VehicleFormComponent } from './ui/vehicle-form/vehicle-form.component';
+import { ImageViewerComponent } from './ui/image-viewer/image-viewer.component';
 
 const PIPES = [
   SearchFilterPipe]
 
 const COMPONENTS = [
   HeaderComponent,
-  ImageComponent,
-  TakePictureComponent, 
+  ImageViewerComponent,
+  TakePictureComponent,
   OlMapComponent,
-  PersonFormComponent, 
-  AddressFormComponent]
+  PersonFormComponent,
+  VehicleFormComponent,
+  SearchAddressComponent,
+  ItemTitleComponent]
 
 const MODULES = [
   CommonModule,
@@ -34,6 +38,7 @@ const PROVIDERS = []
     ...COMPONENTS,
     ...PIPES
   ],
+  entryComponents: [ImageViewerComponent],
   imports: [
     ...MODULES
   ],
