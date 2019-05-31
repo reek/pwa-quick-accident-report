73d238418c5c8969294c7b9eb7b9a7eae3215bb2
reves <ricardo.reves@gmail.com>
2019-05-31 14:38:32 +0200

    cleanup

--- src/app/shared/ui/take-picture/take-picture.component.ts
+++ src/app/shared/ui/take-picture/take-picture.component.ts
@@ -1,9 +1,8 @@
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { CameraService } from 'src/app/core/services/camera/camera.service';
-import { IPicture } from '../../models/picture/picture';
-import * as Tesseract from 'tesseract.js'
-
+import { DomSanitizer } from '@angular/platform-browser';
+import { ITakePicture } from '../../models/take-picture/take-picture';
 
 @Component({
   selector: 'app-take-picture',
@@ -13,15 +12,17 @@ import * as Tesseract from 'tesseract.js'
 export class TakePictureComponent implements OnInit {
 
   @Input() public title: string = 'take picture'
-  @Output() public onTaked: EventEmitter<IPicture> = new EventEmitter<IPicture>()
+  @Input() public instructions: string = 'take instructions'
+  @Output() public onTaked: EventEmitter<ITakePicture> = new EventEmitter<ITakePicture>()
 
   public form: FormGroup
-  public imageUrl: any = "assets/images/aperture.png"
+  public imageUrl: string = "assets/images/aperture.svg"
   public retry: boolean = false
 
   constructor(
     private formBuilder: FormBuilder,
-    private cameraService: CameraService) {
+    private cameraService: CameraService,
+    private sanitizer: DomSanitizer) {
   }
 
   public ngOnInit() {
@@ -53,38 +54,21 @@ export class TakePictureComponent implements OnInit {
     })
   }
 
-  public async onTake(fake: boolean = false) {
-    if (fake) {
-      //this.imageUrl = await fetch("https://loremflickr.com/300/200/car&_=" + Date.now()).then(res => res.url)
-      const blob = await fetch("https://loremflickr.com/300/200/car&_=" + Date.now()).then(res => res.blob()).catch(err => err);
-      this.imageUrl = await this.blobToBase64(blob).then(base64 => base64).catch(err => err);
+  public async onTake(useFake: boolean = false) {
+    if (useFake) {
+      const imageBlob = await fetch("https://picsum.photos/400/200/?_=" + Date.now()).then(res => res.blob()).catch(err => err);
+      this.imageUrl = await this.blobToBase64(imageBlob).then(base64Url => base64Url).catch(err => err);
     } else {
-      this.imageUrl = await this.cameraService.takePicture()
+      this.imageUrl = await this.cameraService.takePicture() || this.imageUrl
     }
     this.form.get('imageUrl').setValue(this.imageUrl);
-
-    /*     
-    const blob = await this.base64toBlob(this.imageUrl)
-        console.log(5, this.imageUrl, blob)
-    
-        const tesseract = Tesseract.create({
-          langPath: '/assets/lib/fra.traineddata',
-          corePath: '/assets/lib/index.js',
-          workerPath: '/assets/lib/worker.js',
-        });
-    
-        tesseract.recognize(blob)
-          .progress(message => console.log(message))
-          .catch(err => console.error(err))
-          .then(result => console.log(result))
-          .finally(resultOrError => console.log(resultOrError)) 
-          */
     this.retry = true
+    console.log(this.imageUrl)
   }
 
   public onSubmit() {
     if (this.form.valid) {
-      const value: IPicture = this.form.value
+      const value: ITakePicture = this.form.value
       this.onTaked.emit(value)
       console.info('TakePictureform');
     }
