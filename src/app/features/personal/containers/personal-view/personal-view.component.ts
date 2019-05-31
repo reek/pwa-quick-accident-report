357b9653334728c7385ea0dacdfe26e394d051c3
reves <ricardo.reves@gmail.com>
2019-05-31 14:43:37 +0200

    refatorize

--- src/app/features/personal/containers/personal-view/personal-view.component.ts
+++ src/app/features/personal/containers/personal-view/personal-view.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { PersonalService } from '../../personal.service';
-import { IPersonal } from 'src/app/shared/models/personal/personal';
-import { IAddress } from 'src/app/shared/models/address/address';
-import { Observable } from 'rxjs';
+import { UserService } from '../../../../core/services/user/user.service';
+import { IPerson } from 'src/app/shared/models/person/person';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
-import { map } from 'rxjs/operators';
-
-
 @Component({
   selector: 'app-personal-view',
   templateUrl: './personal-view.component.html',
@@ -16,38 +12,20 @@ export class PersonalViewComponent implements OnInit {
 
   public payload: any[] = []
   public step: number = 1
-
-  public addressData$: Observable<IAddress>;
-  public personData$: Observable<IPersonal>;
+  public userPersonalData$: Observable<IPerson>
 
   constructor(
     private router: Router,
-    private personalService: PersonalService) {
+    private userService: UserService) {
   }
 
   public ngOnInit() {
-    this.personData$ = this.personalService.getPersonal()
-    this.addressData$ = this.personData$.pipe(map((data: IPersonal) => data.address))
+    this.userPersonalData$ = this.userService.getUserPersonal()
   }
 
-  public onNext(data: any): void {
-    this.payload.push(data)
-    this.step += 1
-    console.log('push', data, this.payload)
-  }
-
-  public onBack(): void {
-    if (this.step > 0)
-      this.step -= 1
-  }
-
-  public onSave(): void {
-    console.log('save personal data', this.payload)
-    const payload: IPersonal = Object.assign({}, ...this.payload)
-    this.personalService.updatePersonal(payload).subscribe((res: any) => {
-      if (res.user && res.token)
-        this.router.navigateByUrl("/")
-    })
+  public onSave(payload: any): void {
+    console.log('save user personal data', payload)
+    this.userService.updateUserPersonal(payload)
   }
 
 }
