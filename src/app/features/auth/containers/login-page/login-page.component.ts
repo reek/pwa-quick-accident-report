ea73aab3ada27b7c128ef4eb81d18e76167bea22
reves <ricardo.reves@gmail.com>
2019-05-31 14:35:21 +0200

    add env variable to login form builder add unsubscribe to onLogin()

--- src/app/features/auth/containers/login-page/login-page.component.ts
+++ src/app/features/auth/containers/login-page/login-page.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../auth.service';
 import { Router } from '@angular/router';
 import { ILogin } from 'src/app/shared/models/auth/auth';
 import { ValidateIsEmailValid } from 'src/app/shared/validators/field.validator';
 import { NotifyService } from 'src/app/core/services/notify/notify.service';
+import { environment as env } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   public title: string = "Login"
   public form: FormGroup
@@ -26,28 +27,29 @@ export class LoginPageComponent implements OnInit {
     this.buildForm()
   }
 
+  public ngOnDestroy() {
+    // AutoUnsubscribe
+  }
+
   public buildForm() {
     this.form = this.formBuilder.group({
-      email: ['test@mail.com', [ValidateIsEmailValid]],
-      password: ['1234', [Validators.minLength(4)]]
+      email: [env.loginEmail, [ValidateIsEmailValid]],
+      password: [env.loginPassword, [Validators.minLength(4)]]
     })
   }
 
   public onLogin() {
     if (this.form.valid) {
-      console.log('form login submitted');
+      console.log(this.title, 'form submitted');
       const payload: ILogin = this.form.value
-      this.authService.login(payload).subscribe((res: any) => {
-        console.log('login response', res)
+      const subs = this.authService.login(payload).subscribe((res: any) => {
+        console.log(this.title, 'response', res)
         if (res.user && res.token) {
           return this.router.navigateByUrl("/")
         }
-      }, err => this.notifyService.show("Bad Login 😞"));
+      }, (err: any) => this.notifyService.show("Sorry, bad login 😞"),
+        () => subs.unsubscribe());
     }
-  }
-
-  public onReset() {
-    this.form.reset();
   }
 
 }
