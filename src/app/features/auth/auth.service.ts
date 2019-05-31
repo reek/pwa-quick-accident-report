3083a96f6b06f6a2e0e4aed26802fc42a6cb2a0d
reves <ricardo.reves@gmail.com>
2019-05-31 14:50:30 +0200

    update  logout()

--- src/app/features/auth/auth.service.ts
+++ src/app/features/auth/auth.service.ts
@@ -2,7 +2,7 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap, map, catchError } from 'rxjs/operators';
-import { ILogin, IRegister, IForgot, IEmail } from 'src/app/shared/models/auth/auth';
+import { ILogin, IRegister, IForgotPassword, IResetPassword, IEmail } from 'src/app/shared/models/auth/auth';
 import { environment as env } from 'src/environments/environment';
 import { Router } from '@angular/router';
 import { NotifyService } from 'src/app/core/services/notify/notify.service';
@@ -34,7 +34,6 @@ export class AuthService {
     return this.http.get(`${env.apiEndpoint}/users`)
       .pipe(
         map((res: any) => {
-          console.log(res)
           if (res.user && res.token) {
             this.userData.next(res.user)
             this.userLogged.next(true)
@@ -44,13 +43,14 @@ export class AuthService {
             return false
           }
         }),
-        catchError((_: any) => (console.log(_), of(false)))
+        catchError((err: any) => (console.log("isLogged", err), of(false)))
       )
   }
 
   public logout() {
     localStorage.setItem(env.authTokenStorageKey, null)
     this.userLogged.next(false)
+    this.userData.next({})
     this.router.navigateByUrl("/auth/login")
   }
 
@@ -83,15 +83,16 @@ export class AuthService {
       )
   }
 
-  public forgot(payload: IForgot) {
-    return this.http.post(`${env.apiEndpoint}/auth/forgot`, payload)
-      .pipe(
-        tap(_ => console.log(_))
-      )
+  public forgotPassword(payload: IForgotPassword) {
+    return this.http.post(`${env.apiEndpoint}/auth/forgot/password`, payload)
+  }
+
+  public resetPassword(payload: IResetPassword) {
+    return this.http.post(`${env.apiEndpoint}/auth/reset/password`, payload)
   }
 
   public checkEmailTaken(payload: IEmail) {
-    return this.http.post(`${env.apiEndpoint}/auth/check/email`, payload)
+    return this.http.post(`${env.apiEndpoint}/auth/email/taken`, payload)
   }
 
   public getUserData(): Observable<any> {
