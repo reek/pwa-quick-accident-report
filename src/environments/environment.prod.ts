fd8dda42f16c8da7e36476dd068fa8228b18bb94
reves <ricardo.reves@gmail.com>
2019-05-31 15:32:31 +0200

    add environment prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiEndpoint: "https://quiet-sands-22246.herokuapp.com/api",
+  authTokenStorageKey: "tJrmvvEJGazZYOHzWvIX",
+  loginEmail: "",
+  loginPassword: "",
+};
